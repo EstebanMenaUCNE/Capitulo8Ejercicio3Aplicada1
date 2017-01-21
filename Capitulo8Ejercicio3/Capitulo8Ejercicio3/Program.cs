4848c6467a332f7f6816b5600a01347efed94014
DESKTOP-4V48SB7\Equipo <esteban.smb.3008@gmail.com>
2017-01-21 15:33:43 -0400

    Listo

---
+++ Capitulo8Ejercicio3/Capitulo8Ejercicio3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Capitulo8Ejercicio3
+{
+    class Program
+    {
+        //Hacer un programa que muestre la hora del día en formato AM/FM seguida del año, el día y el mes actual.
+        static void Main(string[] args)
+        {
+            DateTime horaFecha = DateTime.Now;
+            Console.WriteLine("La hora y fecha actuales son: {0: h:mm tt yyyy/m/d}", horaFecha);
+            Console.ReadKey();
+        }
+    }
+}
